67469943252d7481e7164404368993f672824fc2
Luram Archanjo <luram.archanjo@sensedia.com>
2019-09-22 19:50:37 -0300

    Testing Strings New Methods

---
+++ README.md
@@ -0,0 +1,9 @@
+# Overview
+
+The Java 11 was release at 09/2018 and the main features are:
+
+- Strings New Methods
+
+The full release notes is [here]
+
+[here]: https://www.oracle.com/technetwork/java/javase/11-relnotes-5012447.html
---
+++ src/main/java/com/example/StringMethodExample.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class StringMethodExample {
+
+  public static void main(String[] args) {
+    System.out.println(" ".isBlank());
+    System.out.println("".isBlank());
+    System.out.println(" ".isEmpty());
+    System.out.println("".isEmpty());
+  }
+
+}
