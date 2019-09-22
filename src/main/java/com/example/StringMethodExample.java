48ef6e2326cbe2be6070722fbca85801d42c7a12
Luram Archanjo <luram.archanjo@sensedia.com>
2019-09-22 19:54:28 -0300

    Testing Strings New Methods

--- src/main/java/com/example/StringMethodExample.java
+++ src/main/java/com/example/StringMethodExample.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import java.util.stream.Collectors;
+
 public class StringMethodExample {
 
   public static void main(String[] args) {
@@ -7,6 +9,22 @@ public class StringMethodExample {
     System.out.println("".isBlank());
     System.out.println(" ".isEmpty());
     System.out.println("".isEmpty());
+
+    System.out.println("1\n2\n3".lines().collect(Collectors.toList()));
+
+    /**
+     * But we already have trim(). Then what’s the need of strip()?
+     * strip() is “Unicode-aware” evolution of trim().
+     *
+     * When trim() was introduced, Unicode wasn’t evolved. Now,
+     * the new strip() removes all kinds of whitespaces leading and trailing(check the
+     * method Character.isWhitespace(c) to know if a unicode is whitespace or not)
+     */
+    System.out.println("           luram             ".strip());
+    System.out.println("           luram             ".stripLeading());
+    System.out.println("           luram             ".stripTrailing());
+
+    System.out.println("=".repeat(5));
   }
 
 }
